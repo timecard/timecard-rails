430777e675c27723b55ce24228111b7bcc101a5f
pandeiro245 <nishiko@mindia.jp>
2013-10-26 20:38:53 +0900

    refs #10

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -3,16 +3,11 @@ require 'wheel/concerns/store_into'
 class Provider < ActiveRecord::Base
   include Wheel::Concerns::StoreInto
 
-  def self.github
-    #TODO これだと複数人で運用している場合も同じ人のoauth_tokenが利用されてしまうので議論の余地あり
-    # （実行者はgithubのoauthを持たないこと場合も想定される）
-    # https://ruffnote.com/timecard/timecard/3412
+  def self.github(token)
     Github.new(
       :client_id => SERVICES['github']['key'],
       :client_secret => SERVICES['github']['secret'],
-      :oauth_token => Authentication.find_by(
-        "oauth_token is not null"
-      ).oauth_token 
+      :oauth_token => token
     )
   end
 end
