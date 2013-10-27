012f790d7cc494210a771bf0355064a8becb5274
Kota Miyake <miyake.kota@gmail.com>
2013-10-27 11:22:35 +0900

    コード整形

--- app/controllers/work_logs_controller.rb
+++ app/controllers/work_logs_controller.rb
@@ -2,12 +2,9 @@ class WorkLogsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_work_log, only: [:edit, :update, :destroy, :stop]
 
-  # GET /work_logs/1/edit
   def edit
   end
 
-  # PATCH/PUT /work_logs/1
-  # PATCH/PUT /work_logs/1.json
   def update
     respond_to do |format|
       if @work_log.update(work_log_params)
@@ -20,8 +17,6 @@ class WorkLogsController < ApplicationController
     end
   end
 
-  # DELETE /work_logs/1
-  # DELETE /work_logs/1.json
   def destroy
     @work_log.destroy
     respond_to do |format|
@@ -55,13 +50,12 @@ class WorkLogsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_work_log
-      @work_log = WorkLog.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def work_log_params
-      params.require(:work_log).permit(:start_at, :end_at, :issue_id, :user_id)
-    end
+  def set_work_log
+    @work_log = WorkLog.find(params[:id])
+  end
+
+  def work_log_params
+    params.require(:work_log).permit(:start_at, :end_at, :issue_id, :user_id)
+  end
 end
