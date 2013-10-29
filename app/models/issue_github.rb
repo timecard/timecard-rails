2439de5fe26b0daa53d961e0b0b3c42c88c1e0b6
Kota Miyake <miyake.kota@gmail.com>
2013-10-29 14:57:10 +0900

    Github Issueのassigneeを変更できる

--- app/models/issue_github.rb
+++ app/models/issue_github.rb
@@ -20,15 +20,16 @@ class IssueGithub < Provider
 
   def modify(token, params)
     options = {}
-    if params[:assignee_id] && params[:assignee_id].blank?
-      option["assignee"] = nil
-    elsif params[:assign_id]
-      a = User.find(params[:assignee_id])
-      option["assignee"] = a.github.username
-    end
     options["title"] = params[:subject] if params[:subject]
     options["body"] = params[:description] if params[:description]
     options["state"] = params[:state] if params[:state]
+    if params[:assignee_id].present?
+      user = User.find(params[:assignee_id])
+      options["assignee"] = user.github.username
+    else
+      options["assignee"] = nil
+    end
+    # FIXME full_nameを保存するときにスペースは取り除くべき
     fn = self.issue.project.github.full_name.gsub(/  /,"").split("/")
     issue = Provider.github(token).issues.edit(
       fn[0], fn[1], self.number, options
@@ -36,6 +37,7 @@ class IssueGithub < Provider
   end
 
   def add_comment(token, params)
+    # FIXME full_nameを保存するときにスペースは取り除くべき
     fn = self.issue.project.github.full_name.gsub(/  /,"").split("/")
     comment = Provider.github(token).issues.comments.create(
       fn[0], fn[1], self.number, body: params[:body]
