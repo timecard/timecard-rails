1442222e28e6be3fa5bfabefb36aff19a41833c8
Kota Miyake <miyake.kota@gmail.com>
2013-10-30 10:40:32 +0900

    export時のjson生成にjbuilderを使用する及びbugfix

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -2,51 +2,10 @@ class DataController < ApplicationController
   def show
     raise unless current_user
     raise if Member.where(user_id: current_user.id, project_id: params[:id].to_i).blank?
-    res = {}
-    project = Project.find(params[:id])
-    res["name"] = project.name
-    res["issues"] = project.issues.map do |issue|
-      iss = JSON.parse(issue.to_json)
-      iss["author_email"] = issue.author.email
-      iss.delete("author_id")
-      iss["created_at"] = issue.created_at.to_i
-      iss["updated_at"] = issue.updated_at.to_i
-
-      iss["workloads"] = Workload.where(issue_id: issue["id"]).map{|wl|
-        wl_hash = JSON.parse(wl.to_json)
-        wl_hash["start_at"] = wl.start_at.to_i
-        wl_hash["end_at"] = wl.end_at.to_i
-        wl_hash["user_email"] = wl.user.email
-        wl_hash.delete("user_id")
-        wl_hash
-      }.map{|wl|
-        wl.delete("id")
-        wl.delete("issue_id")
-        wl.delete("created_at")
-        wl.delete("updated_at")
-        wl
-      }
-
-      iss["comments"] = Comment.where(issue_id: issue["id"]).map do |comment|
-        comment_hash = JSON.parse(comment.to_json)
-        comment_hash["body"] = comment.body
-        comment_hash["user_email"] = comment.user.email
-        comment_hash
-      end.map do |comment|
-        comment.delete("id")
-        comment.delete("user_id")
-        comment.delete("issue_id")
-        comment.delete("created_at")
-        comment.delete("updated_at")
-        comment
-      end
-
-      iss.delete("id")
-      iss.delete("project_id")
-      iss
+    @project = Project.find(params[:id])
+    respond_to do |format|
+      format.json { render "show", status: :created, location: @project }
     end
-
-    render json: res.to_json
   end
 
   def create
@@ -58,9 +17,12 @@ class DataController < ApplicationController
       @project.add_github("#{pjtg["owner_login"]}/#{pjtg["name"]}")
     end
     pjt["issues"].each do |iss|
-      issue = Issue.new(subject: iss["subject"])
+      issue = Issue.new(subject: iss["subject"], description: iss["description"])
       issue.created_at = Time.at(iss["created_at"])
-      issue.author_id = User.find_or_create_by(email: iss["author_email"]).id
+      issue.updated_at = Time.at(iss["updated_at"])
+      # TODO ユーザー情報の引き継ぎが必要
+      author = User.find_by(email: iss["author_email"]) || User.new(email: iss["author_email"]).save(validate: false) ? User.find_by(email: iss["author_email"]) : nil
+      issue.author_id = author.id
       issue.project_id = @project.id
       issue.save
 
