904b7c7c2cc58ad81aa3d6261522da6053361e56
Kota Miyake <miyake.kota@gmail.com>
2013-11-09 18:37:19 +0900

    github経由で新規登録してもパスワードが変更できる
    current passwordをバイパスしてデータの更新を行うように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,10 +15,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:name, :email, :password, :password_confirmation, :current_password)
+      u.permit(:name, :email, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:name, :email, :password, :password_confirmation, :current_password)
+      u.permit(:name, :email, :password, :password_confirmation)
     end
   end
 end
