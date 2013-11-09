058a9c78fee92acb39f85c3462f9acae551bd1c5
Kota Miyake <miyake.kota@gmail.com>
2013-11-09 18:12:02 +0900

    壊れていたテストを一部修正

--- spec/factories/issues.rb
+++ spec/factories/issues.rb
@@ -1,11 +1,9 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :issue do
     subject "MyString"
     description "MyText"
-    start_date "2013-09-30 22:53:40"
-    integer "MyString"
+    will_start_at "2013-09-30 22:53:40"
+    status 1
     closed_on "2013-09-30 22:53:40"
     project_id 1
     author_id 1
