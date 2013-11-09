82260643f5333982e9ab6a4cbfda160ab8020818
Kota Miyake <miyake.kota@gmail.com>
2013-11-09 18:37:19 +0900

    github経由で新規登録してもパスワードが変更できる
    current passwordをバイパスしてデータの更新を行うように修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,30 +1,21 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def update
-    @user = User.find(current_user.id)
+    account_update_params = devise_parameter_sanitizer.sanitize(:account_update)
 
-    successfully_updated = if needs_password?(@user, params)
-      @user.update_with_password(devise_parameter_sanitizer.sanitize(:account_update))
-    else
-      params[:user].delete(:current_password)
-      @user.update_without_password(devise_parameter_sanitizer.sanitize(:account_update))
+    # required for settings form to submit when password is left blank
+    if account_update_params[:password].blank?
+      account_update_params.delete("password")
+      account_update_params.delete("password_confirmation")
     end
 
-    if successfully_updated
+    @user = User.find(current_user.id)
+    if @user.update(account_update_params)
       set_flash_message :notice, :updated
+      # Sign in the user bypassing validation in case his password changed
       sign_in @user, :bypass => true
       redirect_to after_update_path_for(@user)
     else
       render "edit"
     end
-  end
-
-  private
-
-  # check if we need password to update user data
-  # ie if password or email was changed
-  # extend this as needed
-  def needs_password?(user, params)
-    user.email != params[:user][:email] ||
-      params[:user][:password].present?
   end
 end
