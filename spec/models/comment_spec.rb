99a13ffb798330cd6d38fb32b910390140698921
Kota Miyake <miyake.kota@gmail.com>
2013-11-19 13:19:19 +0900

    Add spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#github" do
+    it "should be return CommentGithub" do
+      comment = create(:comment)
+      comment.add_github(1)
+      expect(comment.github.class).to be(CommentGithub)
+    end
+  end
+
+  describe "#add_github" do
+    describe "with valid params" do
+      it "should be return true" do
+        comment = create(:comment)
+        expect(comment.add_github(1)).to be_true
+      end
+    end
+  end
 end
