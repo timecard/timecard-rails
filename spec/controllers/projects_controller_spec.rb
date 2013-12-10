5fc372afcb7b1a5c72264ec6a0c228b719316675
Kota Miyake <miyake.kota@gmail.com>
2013-12-10 15:09:47 +0900

    Add test in order to increase coverage

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,4 +1,86 @@
 require 'spec_helper'
 
 describe ProjectsController do
+  before do
+    @user = create(:user)
+    sign_in @user
+  end
+
+  describe "GET 'show'" do
+    before do
+      @project = create(:public_active_project)
+    end
+
+    it "returns http status ok" do
+      get 'show', id: @project.to_param
+      expect(response).to be_ok
+    end
+
+    it "renders template 'show'" do
+      get 'show', id: @project.to_param
+      expect(response).to render_template 'show'
+    end
+  end
+
+  describe "GET 'new'" do
+    it "returns http status ok" do
+      get 'new'
+      expect(response).to be_ok
+    end
+
+    it "renders template 'new'" do
+      get 'new'
+      expect(response).to render_template 'new'
+    end
+  end
+  
+  describe "POST 'create'" do
+    context "with valid params" do
+      it "redirects to project" do
+        post 'create', project: attributes_for(:public_active_project)
+        expect(response).to redirect_to Project.last
+      end
+
+      it "creates a project" do
+        expect {
+          post 'create', project: attributes_for(:public_active_project)
+        }.to change(Project, :count).by(1)
+      end
+    end
+
+    context "with invalud params" do
+      it "renders template 'new'" do
+        post 'create', project: attributes_for(:public_active_project, name: nil)
+        expect(response).to render_template 'new'
+      end
+    end
+  end
+
+  describe "PUT/PATCH 'update'" do
+    before do
+      @project = create(:public_active_project, name: "Project")
+      @project.members.create(user: @user, is_admin: true)
+      @name = "Name cahnges"
+    end
+
+    context "with valid params" do
+      it "redirects to project" do
+        patch 'update', id: @project.to_param, project: { name: @name }
+        expect(response).to redirect_to @project
+      end
+
+      it "updates a project" do
+        patch 'update', id: @project.to_param, project: { name: @name }
+        @project.reload
+        expect(@project.name).to eq(@name)
+      end
+    end
+
+    context "with invalid params" do
+      it "renders template 'edit'" do
+        patch 'update', id: @project.to_param, project: { name: nil }
+        expect(response).to render_template 'edit'
+      end
+    end
+  end
 end
---
+++ spec/models/provider_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Provider do
+  describe ".github" do
+    it "returns Github Client object" do
+      expect(Provider.github("token").class).to be(Github::Client)
+    end
+  end
+
+  describe ".ruffnote" do
+    it "returns Github Client object" do
+      expect(Provider.ruffnote("token").class).to be(OAuth2::AccessToken)
+    end
+  end
+end
