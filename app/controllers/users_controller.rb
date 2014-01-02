e16fcab57bd4c7f0eaf618a187cac36f29bcf940
Kota Miyake <miyake.kota@gmail.com>
2014-01-02 19:04:41 +0800

    Add dashboard page
    Move content from users#show to dashboards#show.
    dashboards#show only can see in myself.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
 
   def show
-    @user = User.find(params[:id])
-    @workloads = @user.workloads.complete.where("start_at >= ? AND start_at <= ?", Time.now.beginning_of_day, Time.now.end_of_day)
-    respond_to do |format|
-      format.html
-    end
   end
 
   def disconnect
