073e53b59b81c2db11f8c5442655cec94b9787c2
Kota Miyake <miyake.kota@gmail.com>
2014-01-03 18:14:24 +0800

    Show only projects that has issues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,11 @@ class HomeController < ApplicationController
     if user_signed_in?
       public_projects = Project.public.active.where_values.reduce(:and)
       my_projects = Project.active.visible(current_user).where_values.reduce(:and)
-      @projects = Project.where(public_projects.or(my_projects))
-      @issues = Issue.with_status("open").where("assignee_id = ?", current_user.id)
+      @projects = Project.where(public_projects.or(my_projects)).
+      select("projects.*, count(issues.id) AS issues_count").
+      joins(:issues).
+      where("issues.assignee_id = #{current_user.id}").
+      group("projects.id")
     else
       render 'welcome'
     end
