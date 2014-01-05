03a41af76738090ca4378e58ba6be389bfa213bd
Hironobu Nishikokura <nishiko@mindia.jp>
2014-01-05 15:29:48 +0900

    change room_id of chatwork

--- app/models/chatwork.rb
+++ app/models/chatwork.rb
@@ -2,7 +2,6 @@ class Chatwork
   def self.post body
     base_url = 'https://api.chatwork.com/v1'
     room_id = 16920606
-    room_id = 24680
     url = "/rooms/#{room_id}/messages"
     token = Authentication.get_chatwork_token
     `curl -X POST -H "X-ChatWorkToken: #{token}" -d "body=#{body}" "#{base_url}#{url}"`
