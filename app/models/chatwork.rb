bbb481ef5f2d75eb38ef564f05df67a765d60c9a
Hironobu Nishikokura <nishiko@mindia.jp>
2014-01-05 15:28:15 +0900

    refs #133 チケットが追加されただけでもchatworkで通知する

---
+++ app/models/chatwork.rb
@@ -0,0 +1,10 @@
+class Chatwork
+  def self.post body
+    base_url = 'https://api.chatwork.com/v1'
+    room_id = 16920606
+    room_id = 24680
+    url = "/rooms/#{room_id}/messages"
+    token = Authentication.get_chatwork_token
+    `curl -X POST -H "X-ChatWorkToken: #{token}" -d "body=#{body}" "#{base_url}#{url}"`
+  end
+end
