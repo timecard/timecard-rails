f87596f13f9b2c6568ecd6fdc443b91ad66112d7
Kota Miyake <miyake.kota@gmail.com>
2014-01-06 14:59:48 +0800

    Refactoring the process of fetch projects
    Logged in user can see all issue that was assigned and the projects

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,7 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
-      public_projects = Project.public.active.where_values.reduce(:and)
-      my_projects = Project.active.visible(current_user).where_values.reduce(:and)
-      @projects = Project.where(public_projects.or(my_projects)).
-      select("projects.*, count(issues.id) AS issues_count").
+      @projects = Project.active.select("projects.*, count(issues.id) AS issues_count").
       joins(:issues).
       where("issues.assignee_id = #{current_user.id}").
       group("projects.id")
