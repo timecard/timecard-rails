0c0dbfd97e72816b863bc32fd9def25efd4cb995
shirakia <atsuo.shiraki@gmail.com>
2014-01-09 03:30:40 +0000

    Revert "redirect to root_path if access denied by cancan"
    This reverts commit 2fdf327563feb8c751d8b12088dfbd64daf8b91c.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
-  end
-
   helper_method :timer
 
   def timer
