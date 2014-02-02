17f777fe971c134e782a7eb135ceb5925c249471
shirakia <atsuo.shiraki@gmail.com>
2014-02-02 21:31:20 +0900

    use fixed time in workload_spec

--- spec/models/workload_spec.rb
+++ spec/models/workload_spec.rb
@@ -2,10 +2,14 @@ require 'spec_helper'
 
 describe Workload do
   describe "#daily" do
+    before :all do
+      @time = Time.zone.local(2014, 02, 01, 21, 0, 0)
+    end
+
     it "returns workloads on the day" do
-      today_workload = create(:workload, start_at: Time.now.utc, end_at: 1.hour.since(Time.now.utc))
-      last_week_workload = create(:workload, start_at: 1.week.ago(Time.now.utc), end_at: 1.week.ago(1.hour.since(Time.now.utc)))
-      expect(Workload.daily(Date.today)).to eq([today_workload])
+      today_workload = create(:workload, start_at: @time, end_at: 1.hour.since(@time))
+      last_week_workload = create(:workload, start_at: 1.week.ago(@time), end_at: 1.week.ago(1.hour.since(@time)))
+      expect(Workload.daily(@time.to_date)).to eq([today_workload])
     end
   end
 end
