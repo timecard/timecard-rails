635e20d45e759ca752c05198652ee978ad889d82
Kota Miyake <miyake.kota@gmail.com>
2014-07-25 01:40:21 +0800

    Extract get daily workloads process

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,6 +3,6 @@ class ReportsController < ApplicationController
 
   def index
     @workloads = current_user.workloads
-      .complete.where("start_at >= ? AND start_at <= ?", Time.now.beginning_of_day, Time.now.end_of_day)
+      .complete.daily
   end
 end
