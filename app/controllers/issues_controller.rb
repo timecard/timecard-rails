15047a088d6dce9a6a50958bd73a1e5ac5f0160a
Kota Miyake <miyake.kota@gmail.com>
2014-09-11 17:13:42 +0800

    Refactor code
    Projectは必ずロードされる

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -5,18 +5,18 @@ class IssuesController < ApplicationController
   before_action :load_labels, except: [:index, :show, :destroy]
 
   def index
-    if @project.present?
-      if params[:user_id].present?
-        @issues = @project.issues.with_status(params[:status]).where("assignee_id = ?", params[:user_id])
-          .page(params[:page])
-          .per(params[:per_page])
-      else
-        @issues = @project.issues
-          .with_status(params[:status])
-          .includes(:github, :ruffnote, :comments)
-          .page(params[:page])
-          .per(params[:per_page])
-      end
+    if params[:user_id].present?
+      @issues = @project.issues
+      .with_status(params[:status])
+      .where("assignee_id = ?", params[:user_id])
+      .page(params[:page])
+      .per(params[:per_page])
+    else
+      @issues = @project.issues
+      .with_status(params[:status])
+      .includes(:github, :ruffnote, :comments)
+      .page(params[:page])
+      .per(params[:per_page])
     end
 
     respond_to do |format|
