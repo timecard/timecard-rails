b9a6874fad2012626935dfc90d517a9b34f16ba5
Kota Miyake <miyake.kota@gmail.com>
2014-09-25 11:52:44 +0800

    Fix services setting by rails_config

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Load configuration early
+require_relative "initializers/rails_config"
+
 # Initialize the Rails application.
 Timecard::Application.initialize!
