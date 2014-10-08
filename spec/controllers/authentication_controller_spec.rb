2d399469e422ba065dc17f77ce393a5e10a2f0de
Kota Miyake <miyake.kota@gmail.com>
2014-10-08 18:00:11 +0800

    Add test for AuthenticationsController

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -1,5 +1,49 @@
 require "rails_helper"
 
 describe AuthenticationsController do
+  describe "GET 'index'" do
+    it "renders template 'index'" do
+      alice = create(:alice)
+      sign_in alice
+      get :index, user_id: alice.id
+      expect(response).to render_template "index"
+    end
+  end
 
+  describe "GET 'new'" do
+    it "renders template 'new'" do
+      alice = create(:alice)
+      sign_in alice
+      get :new, user_id: alice.id
+      expect(response).to render_template "new"
+    end
+  end
+
+  describe "POST 'create'" do
+    it "creates a authentication" do
+      alice = create(:alice)
+      sign_in alice
+      expect {
+        post :create, user_id: alice.id, authentication: attributes_for(:authentication)
+      }.to change(Authentication, :count).by(1)
+    end
+
+    it "renders template 'new' if validate false" do
+      alice = create(:alice)
+      sign_in alice
+      post :create, user_id: alice.id, authentication: attributes_for(:authentication, username: nil)
+      expect(response).to render_template "new"
+    end
+  end
+
+  describe "DELETE 'destroy'" do
+    it "deletes a authentication" do
+      alice = create(:alice)
+      sign_in alice
+      authentication = create(:authentication, user: alice)
+      expect {
+        delete :destroy, user_id: alice.id, id: authentication.id
+      }.to change(Authentication, :count).by(-1)
+    end
+  end
 end
